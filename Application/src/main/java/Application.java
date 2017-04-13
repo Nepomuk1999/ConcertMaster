cc7828e8a4e404f9568209d721c87b201d04e5db
ChristophRheinberger <crh6731@students.fhv.at>
2017-04-13 16:30:44 +0200

    Merge branch 'application' into domain

--- Application/src/main/java/Application.java
+++ Application/src/main/java/Application.java
@@ -2,6 +2,7 @@ import enums.EventStatus;
 import enums.EventType;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 /**
  * Created by Christoph on 13.04.2017.
@@ -12,8 +13,26 @@ public class Application {
 
     Facade facade = new Facade();
 
-    public Integer addEvent(String name, String description, Timestamp start, Timestamp end, EventType eventType,
-                    EventStatus status, String conductor, String location, double defaultPoints, int instrumentation) {
+    public Integer addEvent(String name, String description, LocalDateTime start, LocalDateTime end, EventType eventType,
+                            EventStatus status, String conductor, String location, double defaultPoints, int instrumentation) {
 
+        EventDuty event = new EventDuty();
+
+        event.setName(name);
+        event.setDescription(description);
+        event.setStartTime(start);
+        event.setEndTime(end);
+        event.setEventType(eventType);
+        event.setEventStatus(status);
+        event.setConductor(conductor);
+        event.setLocation(location);
+        event.setDefaultPoints(defaultPoints);
+        event.setInstrumentation(instrumentation);
+
+        return facade.addEvent(event);
+    }
+
+    public List<EventDuty> getEvents(int month, int year) {
+        return facade.getEvents(month, year);
     }
 }
