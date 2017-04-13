dfdc2cc790ec428ffc2e2ee8169c4c3335616d50
den8632 <dominik.entner@students.fhv.at>
2017-04-13 14:12:36 +0200

    getEvents

---
+++ Domain/src/main/java/team_f/domain/entities/Instrumentation.java
@@ -0,0 +1,53 @@
+package team_f.domain.entities;
+
+/**
+ * Created by dominik on 13.04.17.
+ */
+public class Instrumentation {
+    private int instrumentationID;
+    private int stringInstrumentation;
+    private int woodInstrumentation;
+    private int brassInstrumentation;
+    private int percussionInstrumentation;
+
+
+    public int getInstrumentationID() {
+        return instrumentationID;
+    }
+
+    public void setInstrumentationID(int instrumentationID) {
+        this.instrumentationID = instrumentationID;
+    }
+
+    public int getStringInstrumentation() {
+        return stringInstrumentation;
+    }
+
+    public void setStringInstrumentation(int stringInstrumentation) {
+        this.stringInstrumentation = stringInstrumentation;
+    }
+
+    public int getWoodInstrumentation() {
+        return woodInstrumentation;
+    }
+
+    public void setWoodInstrumentation(int woodInstrumentation) {
+        this.woodInstrumentation = woodInstrumentation;
+    }
+
+    public int getBrassInstrumentation() {
+        return brassInstrumentation;
+    }
+
+    public void setBrassInstrumentation(int brassInstrumentation) {
+        this.brassInstrumentation = brassInstrumentation;
+    }
+
+    public int getPercussionInstrumentation() {
+        return percussionInstrumentation;
+    }
+
+    public void setPercussionInstrumentation(int percussionInstrumentation) {
+        this.percussionInstrumentation = percussionInstrumentation;
+    }
+}
