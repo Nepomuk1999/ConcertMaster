252440f3eedeab100b7becce9b9a2d53f5dc951f
Helmut Rhomberg <hrh5775@students.fhv.at>
2017-04-14 10:48:55 +0200

    fix errors with libraries not found

--- Hibernate/src/main/java/team_f/database_wrapper/facade/Main.java
+++ Hibernate/src/main/java/team_f/database_wrapper/facade/Main.java
@@ -1,23 +1,28 @@
 package team_f.database_wrapper.facade;
 
+import team_f.database_wrapper.database.EventDutyEntity;
+import team_f.database_wrapper.entities.EventStatus;
+import team_f.database_wrapper.entities.EventType;
 import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class Main {
     public static void main(final String[] args) throws Exception {
         EntityManager session = SessionFactory.getSession();
 
         Facade facade = new Facade(session);
-        /*
+
         try {
             System.out.println("Testing to add Entities in Database ...");
 
             LocalDateTime s = LocalDateTime.of(2012,2,11,00,00, 00, 00);
             LocalDateTime e = LocalDateTime.of(2012,2,12,00,00, 00, 00);
 
-            facade.addEvent("TestEvent4", "dies ist ein Test", s, e, EventType.Concert, EventStatus.Unpublished, "Test Conductor", "Opernhaus", 2, 1);
+            /*facade.addEvent("TestEvent4", "dies ist ein Test", s, e, EventType.Concert, EventStatus.Unpublished, "Test Conductor", "Opernhaus", 2, 1);
             facade.addEvent("TestEvent5", "dies ist ein Test", s, e, EventType.Concert, EventStatus.Unpublished, "Test Conductor", "Opernhaus", 2, 1);
             facade.addEvent("New Event5", "dies ist ein Test", s, e, EventType.Concert, EventStatus.Unpublished, "Test Conductor", "Opernhaus", 2, 1);
-
+*/
             System.out.println("TestEvents added...");
             List<EventDutyEntity> events = facade.getEvents(4, 2017);
 
@@ -29,6 +34,5 @@ public class Main {
         }
 
         System.exit(0);
-        */
     }
 }
--- Hibernate/src/main/java/team_f/database_wrapper/facade/SessionFactory.java
+++ Hibernate/src/main/java/team_f/database_wrapper/facade/SessionFactory.java
@@ -1,23 +1,34 @@
 package team_f.database_wrapper.facade;
 
 import org.hibernate.HibernateException;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class SessionFactory {
-    private static final EntityManagerFactory _entityManagerFactory;
+    private static EntityManagerFactory _entityManagerFactory;
 
+    private SessionFactory() {
+    }
+
+    /*
     static {
         try {
             _entityManagerFactory = Persistence.createEntityManagerFactory("sem4_team2");
         } catch (Throwable ex) {
             throw new ExceptionInInitializerError(ex);
         }
-    }
+    }*/
 
     public static EntityManager getSession() throws HibernateException {
+        if(_entityManagerFactory == null) {
+            try {
+                _entityManagerFactory = Persistence.createEntityManagerFactory("sem4_team2");
+            } catch (Throwable ex) {
+                throw new ExceptionInInitializerError(ex);
+            }
+        }
+
         return _entityManagerFactory.createEntityManager();
     }
 }
