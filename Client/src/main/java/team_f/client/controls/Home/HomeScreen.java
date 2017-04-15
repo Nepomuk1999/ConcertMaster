de35eb61cd059d24122088110df0cfd39af7968b
obi5747 <oktay.bilgin@students.fhv.at>
2017-04-15 11:39:28 +0200

    HomeScreen add

--- Client/src/main/java/team_f/client/controls/Home/HomeScreen.java
+++ Client/src/main/java/team_f/client/controls/Home/HomeScreen.java
@@ -1,39 +1,25 @@
 package team_f.client.controls.Home;
 
-/**
- * Created by w7pro on 14.04.2017.
- */
-
-import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
-import javafx.stage.Stage;
 
-//TODO: Integrate to Client as Homescreen
-public class HomeScreen extends Application {
 
-    @Override
-    public void start(Stage primaryStage) {
-        BorderPane borderPane = new BorderPane();
+public class HomeScreen extends BorderPane {
+
+
+    public HomeScreen() {
+
         Image image1 = new Image("Logo Neu.jpg");
+       BackgroundSize bSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
 
-
-        BackgroundSize bSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
-
-        borderPane.setBackground(new Background(new BackgroundImage(image1,
+        this.setBackground(new Background(new BackgroundImage(image1,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.CENTER,
                 bSize)));
 
-        Scene scene = new Scene(borderPane, 1000, 600);
 
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 }
