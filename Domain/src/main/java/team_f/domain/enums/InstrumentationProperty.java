3dade76235a33e4df58a5ad38ae91bccfb434ae2
ChristophRheinberger <crh6731@students.fhv.at>
2017-05-09 14:39:36 +0200

    AddInstrumentation in InstrumentationApplication and InstrumentationFacade

---
+++ Domain/src/main/java/team_f/domain/enums/InstrumentationProperty.java
@@ -0,0 +1,25 @@
+package team_f.domain.enums;
+
+import team_f.domain.interfaces.DomainEntityProperty;
+
+/**
+ * Created by Christoph on 09.05.2017.
+ */
+public enum InstrumentationProperty implements DomainEntityProperty {
+    VIOLIN1,
+    VIOLIN2,
+    VIOLA,
+    VIOLINCELLO,
+    DOUBLEBASS,
+    FLUTE,
+    OBOE,
+    CLARINET,
+    BASSOON,
+    HORN,
+    TRUMPET,
+    TROMBONE,
+    TUBE,
+    KETTLEDRUM,
+    PERCUSSION,
+    HARP
+}
