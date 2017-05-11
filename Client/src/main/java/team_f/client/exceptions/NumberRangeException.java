7c9e9efa16a67c635c5ac4fa143fef671de01efc
Helmut Rhomberg <hrh5775@students.fhv.at>
2017-05-11 17:41:51 +0200

    improve musical work gui with number field fix validate

---
+++ Client/src/main/java/team_f/client/exceptions/NumberRangeException.java
@@ -0,0 +1,11 @@
+package team_f.client.exceptions;
+
+public class NumberRangeException extends Exception {
+    public NumberRangeException() {
+        super();
+    }
+
+    public NumberRangeException(String message) {
+        super(message);
+    }
+}
