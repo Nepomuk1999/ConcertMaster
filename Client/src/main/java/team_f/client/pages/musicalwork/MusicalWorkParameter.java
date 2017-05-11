1a31a064f4b0d81d4db4dffa4d631ecae1f2d8d8
ChristophRheinberger <crh6731@students.fhv.at>
2017-05-11 16:13:39 +0200

    Rewrite of the addMusicalWork in Application Layer and MusicalWork in Server

--- Client/src/main/java/team_f/client/pages/musicalwork/MusicalWorkParameter.java
+++ Client/src/main/java/team_f/client/pages/musicalwork/MusicalWorkParameter.java
@@ -1,4 +1,15 @@
 package team_f.client.pages.musicalwork;
 
+import team_f.jsonconnector.entities.MusicalWork;
+
 public class MusicalWorkParameter {
+    private MusicalWork _musicalWork;
+
+    public MusicalWork getMusicalWord() {
+        return _musicalWork;
+    }
+
+    protected void setMusicalWork(MusicalWork musicalWork) {
+        _musicalWork = musicalWork;
+    }
 }
