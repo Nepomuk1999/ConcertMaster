4b6f5f7d2c9fced04f2f46b43e10cf36abeedf61
ChristophRheinberger <crh6731@students.fhv.at>
2017-05-12 12:15:24 +0200

    AddMusicalWork fixed, now working

--- Domain/src/main/java/team_f/domain/enums/InstrumentationProperty.java
+++ Domain/src/main/java/team_f/domain/enums/InstrumentationProperty.java
@@ -1,20 +1,16 @@
 package team_f.domain.enums;
-
 import team_f.domain.interfaces.DomainEntityProperty;
 
-/**
- * Created by Christoph on 09.05.2017.
- */
 public enum InstrumentationProperty implements DomainEntityProperty {
-    VIOLIN1,
-    VIOLIN2,
-    VIOLA,
-    VIOLINCELLO,
-    DOUBLEBASS,
     FLUTE,
     OBOE,
     CLARINET,
     BASSOON,
+    FIRSTVIOLIN,
+    SECONDVIOLIN,
+    VIOLA,
+    VIOLONCELLO,
+    DOUBLEBASS,
     HORN,
     TRUMPET,
     TROMBONE,
