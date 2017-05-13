3844e247edadab8e0ab9e7d335c85a36d70035c0
Helmut Rhomberg <hrh5775@students.fhv.at>
2017-05-13 14:08:49 +0200

    add email validation to accountLogic refactor logic (has to be implemented) fix maven issue

--- Domain/src/main/java/team_f/domain/logic/PersonLogic.java
+++ Domain/src/main/java/team_f/domain/logic/PersonLogic.java
@@ -1,11 +1,13 @@
 package team_f.domain.logic;
 
 import javafx.util.Pair;
+import org.apache.commons.validator.routines.EmailValidator;
 import team_f.domain.entities.Person;
 import team_f.domain.enums.PersonProperty;
 import team_f.domain.interfaces.EntityLogic;
 import java.util.LinkedList;
 import java.util.List;
+import static team_f.domain.enums.PersonProperty.*;
 
 public class PersonLogic implements EntityLogic<Person, PersonProperty> {
     protected PersonLogic() {
@@ -22,35 +24,39 @@ public class PersonLogic implements EntityLogic<Person, PersonProperty> {
 
                 case FIRSTNAME:
                     if (person.getFirstname() == null) {
-                        resultList.add(new Pair<>(String.valueOf(property.FIRSTNAME), "is empty"));
+                        resultList.add(new Pair<>(String.valueOf(FIRSTNAME), "is empty"));
                     }
 
                     break;
 
                 case LASTNAME:
                     if(person.getLastname() == null){
-                        resultList.add(new Pair<>(String.valueOf(property.LASTNAME), "is empty"));
+                        resultList.add(new Pair<>(String.valueOf(LASTNAME), "is empty"));
                     }
 
                     break;
 
                 case PHONE_NUMBER:
                     if(person.getPhoneNumber() == null){
-                        new Pair<>(String.valueOf(property.PHONE_NUMBER), "is empty");
+                        resultList.add(new Pair<>(String.valueOf(PHONE_NUMBER), "is empty"));
                     }
 
                     break;
 
                 case ADDRESS:
                     if(person.getAddress() == null){
-                        new Pair<>(String.valueOf(property.ADDRESS), "is empty");
+                        resultList.add(new Pair<>(String.valueOf(ADDRESS), "is empty"));
                     }
 
                     break;
 
                 case EMAIL:
                     if(person.getEmail() == null){
-                        new Pair<>(String.valueOf(property.EMAIL), "is empty");
+                        resultList.add(new Pair<>(String.valueOf(EMAIL), "is empty"));
+                    } else {
+                        if(!EmailValidator.getInstance().isValid(person.getEmail())) {
+                            resultList.add(new Pair<>(String.valueOf(EMAIL), "is not valid"));
+                        }
                     }
 
                     break;
