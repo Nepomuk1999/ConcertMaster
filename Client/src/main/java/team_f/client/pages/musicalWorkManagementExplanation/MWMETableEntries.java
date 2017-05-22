132160ccf3b5c21386096263fc3a742e529565a7
paul <pko9487@students.fhv.at>
2017-05-22 11:26:06 +0200

    PDF

--- Client/src/main/java/team_f/client/pages/musicalWorkManagementExplanation/MWMETableEntries.java
+++ Client/src/main/java/team_f/client/pages/musicalWorkManagementExplanation/MWMETableEntries.java
@@ -8,8 +8,8 @@ import javafx.collections.ObservableList;
 public class MWMETableEntries {
     final static public ObservableList data = FXCollections.observableArrayList(
             new MWMETableEntries("1", "ConcertMaster allows you to manage musical works. Click the “Compositions” button on the sidebar. The submenu \nthat lists the available functions will appear. Click “Musical Work Management”."),
-            new MWMETableEntries("2", "You can now add or update musical works by entering their name, composer and instrumentation. You can click \n“Reset” to clear all entries in the fields."),
-            new MWMETableEntries("3", "On the bottom of the page you see a table listing all musical works entered into the musical work management.\nTo edit or delete a musical work click “Delete Selected Work” or “Edit Work” after selecting an entry from the table.")
+            new MWMETableEntries("2", "You can now add or update musical works by entering their name, composer and instrumentation. You can click \n“Clear” to clear all entries in the fields."),
+            new MWMETableEntries("3", "On the bottom of the page you see a table listing all musical works entered into the musical work management.\nTo edit or delete a musical work click “Delete” or “Edit” after selecting an entry from the table. You can also search\nfor musical works by name of the composer.")
     );
 
     final private String fieldNumber;
